f80cb6ae45314fcaff4c8277140517c5bd389e96
Mark <Markymark0224@gmail.com>
2025-03-08 20:30:25 -0600

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,9 @@
+# src/config.py
+OTX_API_KEY = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"  # Updates to Credentials for OTI DirectConnect API
+DB_CONFIG = {                                                                      # Credentials for PostgreSQL Database
+    "dbname": "threat_intel",
+    "user": "XXXXXXXXX",           # Update
+    "password": "XXXXXXXXXX",      # Update
+    "host": "localhost",
+    "port": "5432"
+}
